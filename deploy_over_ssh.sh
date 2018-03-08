4d09fa009572d209fb5bcbb9be61d61295b2b9c5
Alex Garland <alex.d.garland@gmail.com>
2018-03-08 19:51:06 +0000

    Add git private key SCP'ing, ruby-full installation (so gem available)

--- deploy_over_ssh.sh
+++ deploy_over_ssh.sh
@@ -1,10 +1,28 @@
 #! /usr/bin/env bash
 
+for i in "$@"
+do
+case $i in
+    -p=*|--password=*)
+    PASSWORD="${i#*=}"
+    shift
+    ;;
+    -k=*|--gitprivatekey=*)
+    GIT_PRIVATE_KEY="${i#*=}"
+    shift
+    ;;
+    *)
+    ;;
+esac
+done
+
 SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
 LOG_DIR=$SCRIPT_DIR/install_logs
 LOG_FILE=$LOG_DIR/`date '+%Y-%m-%d-%H%M%S'`.log
 
-scp -r "$SCRIPT_DIR/setup-resources/" ec2-desktop:/home/ubuntu
+REMOTE_HOME=ec2-desktop:/home/ubuntu
+scp -r "$GIT_PRIVATE_KEY" $REMOTE_HOME/.ssh/
+scp -r "$SCRIPT_DIR/setup-resources/" $REMOTE_HOME/
 
 mkdir -p $LOG_DIR
-ssh ec2-desktop "export PASSWORD=$1 ; bash ~/setup-resources/ec2_setup.sh" 2>&1 | tee $LOG_FILE
+ssh ec2-desktop "export PASSWORD=$PASSWORD ; bash ~/setup-resources/ec2_setup.sh" 2>&1 | tee $LOG_FILE
