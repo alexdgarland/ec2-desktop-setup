24d0216bd56d46fe3762eab92c6fba94bd6536ac
Alex Garland <alex.d.garland@gmail.com>
2018-03-13 20:24:19 +0000

    Fix bugs in Git setup script

--- git_setup/git_setup.sh
+++ git_setup/git_setup.sh
@@ -7,10 +7,10 @@ scp $GIT_CONFIG_FILE ec2-desktop:.gitconfig
 
 scp ~/.ssh/$GITHUB_PRIVATE_KEY ec2-desktop:.ssh/github_private_key
 ssh ec2-desktop "chmod 700 ~/.ssh/github_private_key"
-scp $( dirname "${BASH_SOURCE[0]}" )/git_ssh_config ~/.ssh/config
+scp $( dirname "${BASH_SOURCE[0]}" )/git_ssh_config ec2-desktop:~/.ssh/config
 
 ssh ec2-desktop "sudo apt-get -y install ruby-full"
 ssh ec2-desktop "sudo gem install bundle"
-scp -r $( dirname "${BASH_SOURCE[0]}" )/github-utils/ ec2-desktop
+scp -r $( dirname "${BASH_SOURCE[0]}" )/github-utils/ ec2-desktop:~
 ssh ec2-desktop "cd ~/github-utils ; bundle install"
-ssh ec2-desktop "mkdir ~/git"
+ssh ec2-desktop "mkdir -p ~/git"
